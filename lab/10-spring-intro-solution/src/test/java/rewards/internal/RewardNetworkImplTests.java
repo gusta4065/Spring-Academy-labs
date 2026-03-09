7a14ee7702a516cb4acde28bc351dbe69e305185
gusta4065 <luizgustavoluciopereira@gmail.com>
2026-03-09 17:15:14 -0300

    🤣

--- lab/10-spring-intro-solution/src/test/java/rewards/internal/RewardNetworkImplTests.java
+++ lab/10-spring-intro-solution/src/test/java/rewards/internal/RewardNetworkImplTests.java
@@ -69,5 +69,8 @@ public class RewardNetworkImplTests {
 		// each distribution should be 4.00 (as both have a 50% allocation)
 		assertEquals(MonetaryAmount.valueOf("4.00"), contribution.getDistribution("Annabelle").getAmount());
 		assertEquals(MonetaryAmount.valueOf("4.00"), contribution.getDistribution("Corgan").getAmount());
+
+
+		 System.out.println("Eu adicionei esse comentario ");
 	}
 }
