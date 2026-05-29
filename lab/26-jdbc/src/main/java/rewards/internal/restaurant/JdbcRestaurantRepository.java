e53a011ce4eb31d1426e59a1d4f4ebd9d132fdb3
gusta4065 <luizgustavoluciopereira@gmail.com>
2026-05-29 11:35:19 -0300

    lAB 26 Incompleto(fazendo o todo-09)

--- lab/26-jdbc/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
+++ lab/26-jdbc/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
@@ -2,6 +2,7 @@ package rewards.internal.restaurant;
 
 import common.money.Percentage;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import rewards.Dining;
 import rewards.internal.account.Account;
 
@@ -38,10 +39,13 @@ import java.sql.SQLException;
 
 public class JdbcRestaurantRepository implements RestaurantRepository {
 
-	private DataSource dataSource;
+	//private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
 
-	public JdbcRestaurantRepository(DataSource dataSource) {
-		this.dataSource = dataSource;
+	public JdbcRestaurantRepository(JdbcTemplate jdbcTemplate) {
+
+		 //this.dataSource = dataSource;
+		 this.jdbcTemplate = jdbcTemplate;
 	}
 
 	public Restaurant findByMerchantNumber(String merchantNumber) {
@@ -49,17 +53,18 @@ public class JdbcRestaurantRepository implements RestaurantRepository {
 				+ " from T_RESTAURANT where MERCHANT_NUMBER = ?";
 		Restaurant restaurant = null;
 
-		try (Connection conn = dataSource.getConnection();
-			 PreparedStatement ps = conn.prepareStatement(sql) ){
-			ps.setString(1, merchantNumber);
-			ResultSet rs = ps.executeQuery();
-			advanceToNextRow(rs);
-			restaurant = mapRestaurant(rs);
-		} catch (SQLException e) {
-			throw new RuntimeException("SQL exception occurred finding by merchant number", e);
-		}
+//		try (Connection conn = dataSource.getConnection();
+//			 PreparedStatement ps = conn.prepareStatement(sql) ){
+//			ps.setString(1, merchantNumber);
+//			ResultSet rs = ps.executeQuery();
+//			advanceToNextRow(rs);
+//			restaurant = mapRestaurant(rs);
+//		} catch (SQLException e) {
+//			throw new RuntimeException("SQL exception occurred finding by merchant number", e);
+//		}
 
-		return restaurant;
+		return jdbcTemplate.queryForObject(sql,
+				  (rs, rowNum) -> mapRestaurant(rs), merchantNumber);
 	}
 
 	/**
